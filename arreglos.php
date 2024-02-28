35883b24621da84824ef52abb000f37fe7927bde
memonsalvo <miguelmonsalvo19@gmail.com>
2024-02-28 03:37:08 -0500

    arreglos

---
+++ arreglos.php
@@ -0,0 +1,9 @@
+<?php
+
+$semana = array('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo');
+$semana[7] = 'falco';
+## forma sencilla de declarar arreglos parecida a js
+$semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+echo $semana[1];
+
+?>
---
+++ constantes.php
@@ -0,0 +1,9 @@
+<?php
+define('PI', 3.14);
+echo PI;
+
+define('NOMBRE', ' mike');
+echo NOMBRE;
+
+
+?>

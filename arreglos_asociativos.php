019515934052a4973eb1c498d515ed1c38ff2330
memonsalvo <miguelmonsalvo19@gmail.com>
2024-02-29 02:27:16 -0500

    arreglos2

---
+++ arreglos_asociativos.php
@@ -0,0 +1,6 @@
+<?php
+$alex = array('telefono' => '304', 'edad' => 26, 'apellido' => 'gremory', 'pais' => 'colombia');
+$alex['telefono'] = 11111;
+echo $alex['telefono'];
+
+?>
---
+++ arreglos_multidimensionales.php
@@ -0,0 +1,6 @@
+<?php
+$amigos = array(array('Alejo', 23), array('cesar', 20), array('Manuel', 20));
+
+echo $amigos[0][1];
+
+?>

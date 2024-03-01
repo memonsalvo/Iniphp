6706f78a0d72638c6176a2c4b94c26f87e50e701
memonsalvo <miguelmonsalvo19@gmail.com>
2024-03-01 04:33:51 -0500

    arreglos3

---
+++ contar_elementos_arreglos.php
@@ -0,0 +1,9 @@
+<?php
+$semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+
+echo count($semana);
+
+$ultimo_dia = count($semana) - 1;
+$semana = [$ultimo_dia];
+
+?>
---
+++ recorrer_arreglo.php
@@ -0,0 +1,24 @@
+<?php
+$semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+        foreach($semana as $sem){
+            echo '<li>' . $sem . '</li>';
+        }
+
+        ?>
+    </ul>
+    
+</body>
+</html>
